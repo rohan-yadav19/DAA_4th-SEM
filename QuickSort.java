24b9f501477074940b1507ab20d4992a8d4407ea
Rohan Yadav <yadavrohan98040@gmail.com>
2025-04-28 23:32:13 +0530

    add:progrm to sort array using quick sort

---
+++ QuickSort.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class QuickSort {
+// QuickSort function
+public static void quickSort(int[] arr, int low, int high) {
+if (low < high) {
+int pivotIndex = partition(arr, low, high);
+quickSort(arr, low, pivotIndex - 1);
+quickSort(arr, pivotIndex + 1, high);
+}
+}
+
+// Partition function
+private static int partition(int[] arr, int low, int high) {
+int pivot = arr[high];
+int i = low - 1;
+
+for (int j = low; j < high; j++) {
+if (arr[j] < pivot) {
+i++;
+swap(arr, i, j);
+}
+}
+swap(arr, i + 1, high);
+return i + 1;
+}
+
+// Swap function
+private static void swap(int[] arr, int i, int j) {
+int temp = arr[i];
+arr[i] = arr[j];
+
+arr[j] = temp;
+}
+
+// Main function
+public static void main(String[] args) {
+int[] arr = { 10, 7, 8, 9, 1, 5 };
+System.out.println("Original Array: " + Arrays.toString(arr));
+
+quickSort(arr, 0, arr.length - 1);
+
+System.out.println("Sorted Array: " + Arrays.toString(arr));
+}
+}
